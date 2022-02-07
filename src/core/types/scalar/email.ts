85bccafb03223a6d4c4d3cd3014f9517178da158
Giovanna Cunha <giovannakelli@gmail.com>
2022-02-06 23:55:37 -0300

    email was added to user

--- src/core/types/scalar/email.ts
+++ src/core/types/scalar/email.ts
@@ -1,13 +1,17 @@
 import * as t from 'io-ts'
+import { withMessage } from 'io-ts-types'
 
 type EmailBrand = {
   readonly Email: unique symbol
 }
 
-export const Email = t.brand(
-  t.string,
-  (value): value is t.Branded<string, EmailBrand> => isEmail(value),
-  'Email',
+export const Email = withMessage(
+  t.brand(
+    t.string,
+    (value): value is t.Branded<string, EmailBrand> => isEmail(value),
+    'Email',
+  ),
+  () => 'Invalid email',
 )
 
 const isEmail = (value: string) => {
--- src/core/types/user.ts
+++ src/core/types/user.ts
@@ -1,5 +1,8 @@
+import { Email } from '@/core/types/scalar'
+import * as t from 'io-ts'
+
 export type User = {
-  email: string
+  email: t.TypeOf<typeof Email>
   token: string
   username: string
   bio: string
