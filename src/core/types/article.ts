7ba6320814ca303d92b9a9ef3d33bbdab61ebd13
Giovanna Cunha <giovannakelli@gmail.com>
2022-02-07 00:20:26 -0300

    codec for comment and profile was added

--- src/core/types/article.ts
+++ src/core/types/article.ts
@@ -1,19 +1,24 @@
 import * as t from 'io-ts'
+import { profileCodec } from './profile'
+import { tagCodec } from './tag'
 
 export const articleCodec = t.type({
   slug: t.string,
   title: t.string,
   description: t.string,
   body: t.string,
+  tagList: t.array(tagCodec),
   createdAt: t.string,
   updatedAt: t.string,
   favorited: t.boolean,
   favoriteCount: t.number,
+  auhtor: profileCodec,
 })
 
 export type Article = t.TypeOf<typeof articleCodec>
 
-export type Articles = {
-  articles: Article[]
-  articlesCount: number
-}
+export const articlesCodec = t.type({
+  articles: t.array(articleCodec),
+  articlesCount: t.number,
+})
+export type Articles = t.TypeOf<typeof articlesCodec>
